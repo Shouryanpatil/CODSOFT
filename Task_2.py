aed7f9c5b1b2c4ce8d41d55c461480b184f2ebd3
Shouryanpatil <156405879+Shouryanpatil@users.noreply.github.com>
2024-02-02 14:34:37 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,44 @@
+# Task 2 - CALCULATOR
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Cannot divide by zero"
+    return x / y
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number:"))
+
+print("Select operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Division")
+
+choice = input("Enter choice (1/2/3/4): ")
+
+if choice == '1':
+    result = add(num1, num2)
+    operation = "Additon"
+elif choice =='2':
+    result = subtract(num1,num2)
+    operation = "Subtraction"
+elif choice =='3':
+    result = multiply(num1, num2)
+    operation = "Multiplication"
+elif choice == '4':
+    result = divide(num1,num2)
+    operation = "Division"
+else:
+    result = "Invalid input"
+    operation = "Unknown"
+
+print(f"{operation} result: {result}")
